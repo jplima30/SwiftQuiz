78180cf1586d10f33c7d2b07f57e870d1f738736
João <jplimadev@gmail.com>
2021-01-03 00:15:59 -0300

    Criação das views, LaunchScreen e estrutura de navegação do app

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftQuiz
+//
+//  Created by jplima on 02/01/21.
+//  Copyright © 2021 jplima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
